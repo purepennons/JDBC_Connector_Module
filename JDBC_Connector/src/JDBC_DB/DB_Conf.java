432664f7575c1ba32419b61b53fcb2e4a0f7d9b8
purepennons <y80811@gmail.com>
2014-05-04 14:50:16 +0800

    DB_Conf, OK! DB_Connect were added some functions.

--- JDBC_Connector/src/JDBC_DB/DB_Conf.java
+++ JDBC_Connector/src/JDBC_DB/DB_Conf.java
@@ -10,13 +10,25 @@ public class DB_Conf {
 	private String useDB = "bill";
 	private String username = "local";
 	private String password = "123";
+	private String encoding = "UTF8";
+	private boolean isUnicode = true;
 	
 	//constructor
 	public DB_Conf(){
 	
 	}
 	
-	//set and get function
+	public DB_Conf(String driver, String type, String d, String port, String dbName, String usr, String pw){
+		this.JDBC_Driver = driver;
+		this.DB_TYPE = type;
+		this.domain = d;
+		this.port = port;
+		this.useDB = dbName;
+		this.username = usr;
+		this.password = pw;
+	}
+	
+	//set/get functions
 	public void setJDBC_Driver(String driver){
 		if(driver != ""){
 			this.JDBC_Driver = driver;
@@ -27,7 +39,7 @@ public class DB_Conf {
 		if(this.JDBC_Driver != ""){
 			return this.JDBC_Driver;
 		}
-		return null;
+		return "";
 	}
 	
 	public void setDB_TYPE(String type){
@@ -40,7 +52,7 @@ public class DB_Conf {
 		if(this.DB_TYPE != ""){
 			return this.DB_TYPE;
 		}
-		return null;
+		return "";
 	}
 	
 	public void setDomain(String d){
@@ -53,7 +65,7 @@ public class DB_Conf {
 		if(this.domain != ""){
 			return this.domain;
 		}
-		return null;
+		return "";
 	}
 	
 	public void setPort(String port){
@@ -66,7 +78,7 @@ public class DB_Conf {
 		if(this.port != ""){
 			return this.port;
 		}
-		return null;
+		return "";
 	}
 	
 	public void setUseDB(String dbName){
@@ -79,7 +91,7 @@ public class DB_Conf {
 		if(this.useDB != ""){
 			return this.useDB;
 		}
-		return null;
+		return "";
 	}
 	
 	public void setUsername(String usr){
@@ -88,46 +100,48 @@ public class DB_Conf {
 		}
 	}
 	
+	public String getUsername(){
+		if(this.username != ""){
+			return this.username;
+		}
+		return "";
+	}
+	
 	public void setPassword(String pw){
 		if(pw != ""){
 			this.password = pw;
 		}
 	}
 	
+	public String getPassword(){
+		if(this.password != ""){
+			return this.password ;
+		}
+		return "";
+	}
 	
+	public void setEncoding(String encode){
+		if(encode != ""){
+			this.encoding = encode;
+		}
+	}
 	
+	public String getEncoding(){
+		if(this.encoding != ""){
+			return this.encoding;
+		}
+		return "";
+	}
 	
+	public void setIsUnicode(boolean flag){
+		this.isUnicode = flag;
+	}
 	
+	public boolean getUseUnicode(){
+		return this.isUnicode;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public String getURL(){
+		return "jdbc:" + this.DB_TYPE + "://" + this.domain + ":" + this.port +  "/" + this.useDB + "?" + "useUnicode=" + this.isUnicode + "&" + "characterEncoding=" + this.encoding;
+	}
 }
--- JDBC_Connector/src/JDBC_DB/DB_Connect.java
+++ JDBC_Connector/src/JDBC_DB/DB_Connect.java
@@ -1,8 +1,74 @@
 package JDBC_DB;
 
-import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DB_Connect {
-	public Connection conn = null;
+	private DB_Conf conf = null;
+	private Connection conn = null;
+	private Statement st = null;
+	private PreparedStatement pst = null;
+	private ResultSet rs = null;
+	
+	//constructor
+	public DB_Connect(){
+		this.conf = new DB_Conf();
+	}
+	
+	public DB_Connect(DB_Conf conf){
+		if(conf != null){
+			this.conf = conf;
+		}
+	}
+	
+	//set/get functions
+	public Connection getConn(){
+		if(this.conn != null){
+			return this.conn;
+		}
+		return null;
+	}
+	
+	public Statement getSt(){
+		if(this.st != null){
+			return this.st;
+		}
+		return null;
+	}
+	
+	public PreparedStatement getPst(){
+		if(this.pst != null){
+			return this.pst;
+		}
+		return null;
+	}
+	
+	public ResultSet getRs(){
+		if(rs != null){
+			return this.rs;
+		}
+		return null;
+	}
+	
+	//main functions
+	public boolean init() throws ClassNotFoundException, SQLException{
+		if(this.conf != null){
+			Class.forName(this.conf.getJDBC_Driver());
+			this.conn = DriverManager.getConnection(this.conf.getURL(), this.conf.getUsername(), this.conf.getPassword());
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+//	public static void main(String[] args){
+//		DB_Conf conf = new DB_Conf();
+//		System.out.println(conf.getURL());
+//	}
 
 }
